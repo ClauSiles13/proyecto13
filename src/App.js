996dee348dbfcefd6dc504ae2642bbae63e0ac9d
Claudia <clausikes13@gmail.com>
2023-07-21 09:51:11 -0500

    v06

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import C02contador from './component/C02contador';
 import C03dobleestado from './component/C03dobleestado';
 import C04variable from './component/C04variable';
 import C05operadorTernario from './component/C05operadorTernario';
+import C06matriz from './component/C06matriz';
+import C07matrizOperaciones from './component/C07matrizOperaciones';
 
 function App() {
   return (
@@ -19,6 +21,10 @@ function App() {
       <C04variable xVariable="Hoy dia es viernes..."/>
       <h1>Componente 5</h1>
       <C05operadorTernario />
+      <h1>Componente 6</h1>
+      <C06matriz />
+      <h1>Componente 7</h1>
+      <C07matrizOperaciones />
     </div>
   );
 }
