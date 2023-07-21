2f5a9f48e6425207210d6e39c9865ec52cfbea78
Claudia <clausikes13@gmail.com>
2023-07-21 08:51:32 -0500

    v03

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import C01component from './component/C01component';
+import C02contador from './component/C02contador';
+import C03dobleestado from './component/C03dobleestado';
+import C04variable from './component/C04variable';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Componente 1</h1>
+      <C01component  />
+      <h1>Componente 2</h1>
+      <C02contador />
+      <h1>Componente 3</h1>
+      <C03dobleestado/>
+      <h1>Componente 4</h1>
+      <C04variable xVariable="Hoy dia es viernes..."/>
     </div>
   );
 }
